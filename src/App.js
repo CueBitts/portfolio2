e51095d693672e620b475cf7eda280e6b0f932d3
Quinlan Hogan <quinlan@redbranchmedia.com>
2022-10-31 14:50:04 -0700

    git init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='about-me'>
+        about me
+      </div>
+      <div className='my-skills'>
+        my skills
+      </div>
+      <div className='my-projects'>
+        my projects
+      </div>
+      <div className='contact-me'>
+        contact me
+      </div>
+    </div>
+  )
+}
+
+export default App;
