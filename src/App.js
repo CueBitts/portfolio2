2aa7741a03484bdf159f6cd4edefb96a5409f202
Quinlan Hogan <quinlanhogan@Quinlans-MacBook-Pro.local>
2022-12-13 11:30:58 -0800

    formatted for responsive

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <div className='about-me'>
-        about me
-      </div>
-      <div className='my-skills'>
-        my skills
-      </div>
-      <div className='my-projects'>
-        my projects
-      </div>
-      <div className='contact-me'>
-        contact me
+      <div className='container'>  
+        <div className='column' id='1'></div>
+        <div className='column' id='2'>
+          <div className='about-me'>
+            about me
+          </div>
+          <div className='my-skills'>
+            my skills
+          </div>
+          <div className='my-projects'>
+            my projects
+          </div>
+          <div className='contact-me'>
+            contact me
+          </div>
+          </div>
+        <div className='column' id='3'></div>
       </div>
     </div>
   )
